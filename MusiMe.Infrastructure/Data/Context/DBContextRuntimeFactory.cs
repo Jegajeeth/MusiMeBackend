9c141a76badf2378dd2b7ad1b54fc7f66b9d2da7
Jegajeeth Manikandan <Jegajeeth.Manikandan@nafinc.com>
2025-07-14 17:21:30 +0530

    Removed the dependency with the API project for the Infrastructure.

---
+++ MusiMe.Infrastructure/Data/Context/DBContextRuntimeFactory.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace MusiMe.Infrastructure.Data.Context
+{
+    public class DBContextRuntimeFactory : IDesignTimeDbContextFactory<DBContext>
+    {
+        public DBContext CreateDbContext(string[] args)
+        {
+            string basePath = Directory.GetCurrentDirectory();
+            var confi = new ConfigurationBuilder()
+                .SetBasePath(basePath)
+                .AddJsonFile("appsettings.json", optional: false)
+                .Build();
+            string connectionString = confi.GetConnectionString("ConnectionString") ?? String.Empty;
+
+            var OptionBuilder = new DbContextOptionsBuilder<DBContext>();
+            OptionBuilder.UseNpgsql(connectionString);
+
+            return new DBContext(OptionBuilder.Options);
+        }
+    }
+}
